ef039408509b339523c3547a334b4e4d4d2d04b0
Abhi Patel <abhipatel@Mac.lan>
2025-11-05 19:14:22 +0530

    basic shell command handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::io::{self, Write};
+use std::process::Command;
+use std::env;
+
+fn current_directory() {
+    match env::current_dir() {
+        Ok(path) => println!("The current working directory is {}", path.display()),
+        Err(e) => eprintln!("The error occured is: {}", e),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    Command::new("clear").status();
+    loop {
+        print!("->");
+        io::stdout().flush().unwrap();
+        let mut command = String::new();
+
+        io::stdin().read_line(&mut command).unwrap();
+        let line = command.trim();
+
+        match line {
+            "exit" | "quit" => break,
+            "ping" => println!("pong"),
+            "dir -c" => {
+                current_directory();
+            }
+            "clear" => {
+                let _ = Command::new("clear").status().unwrap();
+            }
+            
+            _ if !line.is_empty() => {
+                let parts: Vec<&str> = line.split_whitespace().collect();
+                let cmd = parts[0];
+                let args = &parts[1..];
+                match Command::new(cmd).args(args).status() {
+                    Ok(status) => println!("Process exited with: {}", status),
+                    Err(e) => println!("Failed to execute command: {}", e),
+                }
+            }
+            
+            _ => { }
+        }
+    }
 }
