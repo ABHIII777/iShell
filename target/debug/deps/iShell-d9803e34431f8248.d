e120f5879318bb0b6d48009f752736f99478a7e2
Abhi Patel <abhipatel@Mac.lan>
2025-11-05 17:23:32 +0530

    first commit

---
+++ target/debug/deps/iShell-d9803e34431f8248.d
@@ -0,0 +1,5 @@
+/Users/abhipatel/Documents/Developer/iShell/target/debug/deps/iShell-d9803e34431f8248.d: src/main.rs
+
+/Users/abhipatel/Documents/Developer/iShell/target/debug/deps/iShell-d9803e34431f8248: src/main.rs
+
+src/main.rs:
---
+++ target/debug/iShell.d
@@ -0,0 +1,1 @@
+/Users/abhipatel/Documents/Developer/iShell/target/debug/iShell: /Users/abhipatel/Documents/Developer/iShell/src/main.rs
