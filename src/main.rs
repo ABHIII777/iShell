97f34161f014dd849d655f418b0298583df3e554
Abhi Patel <abhipatel@Abhis-MacBook-Air.local>
2025-11-10 15:39:01 +0530

    Adding some more ls commands

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,107 @@
 use std::io::{self, Write};
 use std::process::Command;
 use std::env;
-// use prettytable::{Table, Cell, Row};
 use tabled::{builder::Builder};
 use std::fs;
 use colored::*;
+use std::os::unix::fs::MetadataExt;
+use users::*;
 
 fn current_directory() {
     match env::current_dir() {
         Ok(path) => println!("The current working directory is {}", path.display()),
         Err(e) => eprintln!("The error occured is: {}", e),
     }
+}
+
+
+fn lsl_command() {
+    let mut builder = Builder::new();
+
+    builder.push_record([
+        "Name".green().bold().to_string(),
+        "Type".green().bold().to_string(),
+        "Target".green().bold().to_string(),
+        "Read Only".green().bold().to_string(),
+        "Mode".green().bold().to_string(),
+        "Num Links".green().bold().to_string(),
+        "Inode".green().bold().to_string(),
+        "User".green().bold().to_string(),
+        "Group".green().bold().to_string(),
+        "Size".green().bold().to_string(),
+    ]);
+
+    for entry in fs::read_dir(".").unwrap() {
+        let entries = entry.unwrap();
+        let meta = entries.metadata().unwrap();
+
+        let filename = entries.file_name().to_str().unwrap().to_string();
+        let size = meta.len();
+        let file_type = if meta.is_dir() { "Dir" } else { "File" };
+
+        let inode = meta.ino();
+        let uid = meta.uid();
+        let gid = meta.gid();
+        
+        let num_links = meta.nlink();
+        let mode = meta.mode();
+
+        let read_only = meta.permissions().readonly();
+
+        let user = get_user_by_uid(uid)
+            .map(|u| u.name().to_string_lossy().into_owned())
+            .unwrap_or("unknown".into());
+
+        let group = get_group_by_gid(gid)
+            .map(|g| g.name().to_string_lossy().into_owned())
+            .unwrap_or("unknown".into());
+
+        //if meta.is_symlink() {
+        //   let target = fs::read_link(entry.path()).unwrap();
+        //}
+        //
+        let mut target = String::new();
+        if meta.file_type().is_symlink() {
+            target = fs::read_link(entries.path())
+                .unwrap()
+                .to_string_lossy()
+                .into_owned();
+        }
+
+        builder.push_record([filename, file_type.to_string(), target, read_only.to_string(), mode.to_string(), num_links.to_string(), inode.to_string(), user, group, size.to_string()]);
+    }
+
+    let mut table = builder.build();
+    let main_table = table.with(tabled::settings::Style::modern()).with(tabled::settings::Alignment::left());
+
+    println!("{}", main_table);
+}
+
+
+fn lsa_command() {
+    let mut builder = Builder::default();
+
+    builder.push_record([
+        "Name".cyan().bold().to_string(),
+        "Type".cyan().bold().to_string(),
+        "Size".cyan().bold().to_string(),
+    ]);
+
+    for entry in fs::read_dir(".").unwrap() {
+        let entries = entry.unwrap();
+        let meta = entries.metadata().unwrap();
+
+        let filename = entries.file_name().to_str().unwrap().to_string();
+        let size = meta.len().to_string();
+        let file_type = if meta.is_dir() { "Dir" } else { "File" };
+
+        builder.push_record([filename, file_type.to_string(), size]);
+    }
+
+    let mut table = builder.build();
+    let main_table = table.with(tabled::settings::Style::modern()).with(tabled::settings::Alignment::left());
+
+    println!("{}", main_table);
 }
 
 fn tabled_directory() {
@@ -33,50 +124,11 @@ fn tabled_directory() {
         builder.push_record([filename, file_type.to_string(), size]);
     }
 
-    // let table = builder.build().with(tabled::settings::Style::modern()).with(tabled::settings::Alignment::left());
     let mut table = builder.build();
     let main_table = table.with(tabled::settings::Style::modern()).with(tabled::settings::Alignment::left());
 
     println!("{}", main_table);
 }
-
-// fn print_directorytable() {
-//     let mut table = Table::new();
-
-//     table.set_format(*prettytable::format::consts::FORMAT_BOX_CHARS);
-
-//     table.add_row(Row::new(vec![
-//         Cell::new(&"Name").style_spec("bFb"),
-//         Cell::new(&"Type").style_spec("bFb"),
-//         Cell::new(&"Size").style_spec("bFb"),
-//     ]));
-    
-//     for entry in fs::read_dir(".").unwrap() {
-//         let entries = entry.unwrap();
-//         let meta = entries.metadata().unwrap();
-
-//         let filename = entries.file_name().to_str().unwrap().to_string();
-
-//         let mut name_cell = Cell::new(&filename);
-
-//         if meta.is_dir() { 
-//             name_cell = name_cell.style_spec("bFg");
-//         } else {
-//             name_cell = name_cell.style_spec("bFw");
-//         };
-
-//         let size = meta.len();
-//         let file_type = if meta.is_dir() { "Dir" } else { "File" };
-
-//         table.add_row(Row::new(vec![
-//             name_cell,
-//             Cell::new(file_type),
-//             Cell::new(&size.to_string()),
-//         ]));
-//     }
-
-//     table.printstd();
-// }
 
 fn main() {
     Command::new("clear").status();
@@ -99,9 +151,13 @@ fn main() {
                 tabled_directory();
             }
 
-            // "ls" | "dir" => {
-            //     print_directorytable();
-            // }
+            "ls -a" => {
+                lsa_command();
+            }
+
+            "ls -l" => {
+                lsl_command();
+            }
 
             _ if line.contains("cd ") => {
                 let dir = line.strip_prefix("cd ").unwrap().trim();
---
+++ target/debug/deps/iShell-42814dd8dbd4ebcb.d
@@ -0,0 +1,5 @@
+/Users/abhipatel/Documents/Developer/iShell/target/debug/deps/iShell-42814dd8dbd4ebcb.d: src/main.rs
+
+/Users/abhipatel/Documents/Developer/iShell/target/debug/deps/iShell-42814dd8dbd4ebcb: src/main.rs
+
+src/main.rs:
